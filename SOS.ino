5e0db71257ad248b7eb47579b8741cbfb15aee54
Supachai Ghudkam <56030342@kmitl.ac.th>
2017-03-22 21:06:56 +0700

    Add files via upload

---
+++ SOS.ino
@@ -0,0 +1,47 @@
+int led = 2;
+void setup() {
+  // put your setup code here, to run once:
+pinMode(2,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(led,HIGH);
+delay(1000);
+digitalWrite(led,LOW);
+delay(1000);
+digitalWrite(led,HIGH);
+delay(1000)
+digitalWrite(led,LOW);
+delay(1000)
+digitalWrite(led,HIGH);
+delay(1000)
+digitalWrite(led,LOW);
+delay(1000)
+digitalWrite(led,HIGH);
+delay(3000)
+digitalWrite(led,LOW);
+delay(1000)
+digitalWrite(led,HIGH);
+delay(3000)
+digitalWrite(led,LOW);
+delay(1000)
+digitalWrite(led,HIGH);
+delay(3000)
+digitalWrite(led,LOW);
+delay(1000)
+digitalWrite(led,HIGH);
+delay(1000);
+digitalWrite(led,LOW);
+delay(1000);
+digitalWrite(led,HIGH);
+delay(1000)
+digitalWrite(led,LOW);
+delay(1000)
+digitalWrite(led,HIGH);
+delay(1000)
+digitalWrite(led,LOW);
+delay(1000)
+
+
+}
